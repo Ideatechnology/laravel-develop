69855078f213bfd25ed38617eac6ce8ad879ad9c
harry ridwan <harryridwanramadan@gmail.com>
2016-05-07 10:52:36 +0700

    get started
    get started

---
+++ privasi/resources/views/template/t_index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>App IFS</title>
+    {!! Html::style('assets/material.min.css') !!}
+       {!! Html::script('assets/material.min.js') !!}
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  
+       
+    </head>
+    <body>
+       
+        @yield('content')
+
+
+    </body>
+</html>
